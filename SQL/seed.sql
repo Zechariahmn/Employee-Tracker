e1c161a46e0bd7056a5eedafc1a7df93263bfe7d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-07 16:22:08 -0600

    inserted values into employee table

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -19,3 +19,18 @@ INSERT INTO role (title, salary, department_id)
 VALUES ('Accountant', 125000, 3);
 INSERT INTO role (title, salary, department_id)
 VALUES ('Legal Team Lead', 250000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Mikhail', 'Best', 1, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Mikayla', 'Godwin', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Alexa', 'Hawkins', 3, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Seth', 'Royce', 4, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Perscilla', 'Evans', 1, NULL)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Nathan', 'Adams', 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Kyle', 'Bolt', 3, 2)
